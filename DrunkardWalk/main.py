6b46770f97c0f8be4088852caa2cce3ce4885d26
OffworldAstronaut <rafael.science.amorim@gmail.com>
2025-05-01 19:17:50 -0300

    wip: dispersion plot

--- DrunkardWalk/main.py
+++ DrunkardWalk/main.py
@@ -1,10 +1,10 @@
-from random import random
-import numpy as np
-from matplotlib import pyplot as plt
-from typing import List
-from time import time
+from random import random               # for the drunkard coin toss
+from time import time                   # for file R&W operations
+import numpy as np                      # for general calculations
+from matplotlib import pyplot as plt    # for plotting
+from typing import Tuple, List          # typing
 
-
+# Drunkard class, composition with Sidewalk 
 class Drunkard:
     def __init__(self, coin_p : float) -> None:
         """Creates a new "drunkard" (walker) with some parameters
@@ -39,29 +39,45 @@ class Drunkard:
         
     def get_coin_p(self) -> float:
         return self.coin_p
-    
+
+# Main class - all the simulations occur here
 class Sidewalk:
-    def __init__(self, size: int, coin_p : float=0.50):
+    def __init__(self, size: int, coin_p: float=0.50):
+        """Creates a sidewalk (number line) for the Drunkard to walk on
+
+        Args:
+            size (int): size of the sidewalk
+            coin_p (float, optional): Coin probability of walking to the right. Defaults to 0.50.
+        """
         self.drunkard = Drunkard(coin_p)
         self.size = size
         self.wandering_pos = []
+        self.wandering_std = []
     
     def get_size(self) -> int:
         return self.size
     
-    def wander(self, end_step: int=1_000) -> List[int]:
+    def wander(self, end_step: int=1_000) -> Tuple[List]:
         for _ in range (0, end_step):
             self.wandering_pos.append(self.drunkard.walk())
+            self.wandering_std.append(np.std(self.wandering_pos))
             
-        return self.wandering_pos
+        return (self.wandering_pos, self.wandering_std)
 
     def make_dispersion_plot(self) -> None:
-        ...
+        """Plots the dispersion of the walker for each step taken"""
+        
+        plt.title(f"Random Walk STD (p={self.drunkard.get_coin_p()}; size={self.get_size()})")
+        plt.xlabel("Time (Step)")
+        plt.ylabel("STD")
         
     def make_average_pos_plot(self) -> None:
+        # TODO: this method plots the average position of the wandering for each step
         ...
 
     def make_wandering_plot(self) -> None:
+        """Plots the wandering of the walker from beggining until the end."""
+        
         plt.title(f"Random Walk (p={self.drunkard.get_coin_p()}; size={self.get_size()})")
         plt.xlabel("Time (Step)")
         plt.ylabel("Position")
