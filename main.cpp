c4516ce858e451cefb8e2fbc0d24ae943d374209
OffworldAstronaut <rafael.science.amorim@gmail.com>
2025-08-02 16:34:39 -0300

    initial c++ structure

---
+++ main.cpp
@@ -0,0 +1,25 @@
+/**
+ * @file main.cpp
+ * @author Rafael Amorim (rafael.science.amorim@gmail.com)
+ * @brief Simulation and statistical analysis of Random Walk in Random Environments (RWRE)
+ * @version 0.1
+ * @date 2025-08-02
+ * 
+ * @copyright Copyright (c) 2025
+ */
+
+#include <iostream>     // basic I/O 
+#include <fstream>      // files I/O 
+#include <string>       // string manipulation
+#include <format>       // easy string formatting 
+#include <vector>       // vectors 
+#include <cmath>        // numerical necessities 
+#include <random>       // probabilistic necessities 
+#include <ctime>        // time functions 
+
+using namespace std;
+
+int main(){
+    printf("Hello world");
+    return 0; 
+}
